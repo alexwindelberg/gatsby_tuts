ff5f948f3d53a68e5d615bba67f0223ccd878424
alexwindelberg <alexwindelberg@gmail.com>
2018-12-27 14:46:25 -0500

    Linking between pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,8 @@
 import React from "react"
+/* 
+   Layout components are for sections of a site that you want to share across multiple pages. 
+   For example, Gatsby sites will commonly have a layout component with a shared header and footer. 
+   Other common things to add to layouts include a sidebar, and/or a navigation menu. 
+*/
 
 export default props => <h1>{props.headerText}</h1>
